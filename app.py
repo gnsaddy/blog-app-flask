6859626ff883025147987331940fd91fa2303eb0
gnsaddy <aditya.x510@gmail.com>
2021-02-18 21:39:27 +0530

    Update's

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-from flask import Flask, render_template, json
-from models import BlogPost
+from flask import Flask, render_template, json, request
+from models import BlogPost, db
 
 app = Flask(__name__)
-
-
-def load_data():
-    with open("static/data.json") as f:
-        return json.load(f)
-
-data = load_data()
-
 
 @app.route('/')
 def index():
@@ -17,10 +9,15 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/post')
+@app.route('/post', methods=["GET", "POST"])
 def post():
+    if request.method == "POST":
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
 
-    return render_template("post.html", data=data)
+
+    return render_template("post.html")
 
 
 if __name__ == "__main__":
