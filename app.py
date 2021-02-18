b79a5bab013d8a141e614f6dc9921cb32f3d1063
gnsaddy <aditya.x510@gmail.com>
2021-02-18 20:55:02 +0530

    Update's

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite'
 
 
 def load_data():
