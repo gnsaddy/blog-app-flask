2e804d2ae8ea19139fe58a098d5e43c2b926fa5c
gnsaddy <aditya.x510@gmail.com>
2021-02-18 20:41:32 +0530

    Update's

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 
 app = Flask(__name__)
+
+
+def load_data():
+    with open("static/data.json") as f:
+        return json.load(f)
+
+
+data = load_data()
 
 
 @app.route('/')
 def index():
 
-    return "Hello world"
+    return render_template("index.html")
+
+
+@app.route('/post')
+def post():
+
+    return render_template("post.html", data=data)
 
 
 if __name__ == "__main__":
